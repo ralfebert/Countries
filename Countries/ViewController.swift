678fa28c50e7a3b210c5454627c0ee18e051b794
Ralf Ebert <info@ralfebert.de>
2019-07-25 00:09:57 +0200

    Initial Commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Ralf Ebert on 25.07.19.
+//  Copyright © 2019 Ralf Ebert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
