7c48980c5bf5f003bb047ce9abecb6f82aa5f96a
Ralf Ebert <info@ralfebert.de>
2019-07-28 18:08:13 +0200

    load countries from local json file

---
+++ Countries/Controller/ExampleViewController.swift
@@ -0,0 +1,12 @@
+// (C) 2019, Ralf Ebert - iOS Example Project: Countries
+// License: https://opensource.org/licenses/0BSD
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- Countries/Model/Country.swift
+++ Countries/Model/Country.swift
@@ -1,17 +1,39 @@
 // (C) 2019, Ralf Ebert - iOS Example Project: Countries
 // License: https://opensource.org/licenses/0BSD
 
-struct Country {
+import CoreLocation
+import Foundation
+import UIKit
+
+struct Landmark: Codable {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+    var imageUrl: URL
+    var metadataUrl: URL
+    var copyright: String
+
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+    }
+}
+
+struct Country: Codable {
 
     var iso2: String
     var name: String
+    var landmark: Landmark
 
-    static let allCountries = [
-        Country(iso2: "be", name: "Belgien"),
-        Country(iso2: "bg", name: "Bulgarien"),
-        Country(iso2: "el", name: "Griechenland"),
-        Country(iso2: "lt", name: "Litauen"),
-        Country(iso2: "pt", name: "Portugal"),
-    ]
+    var image: UIImage {
+        return UIImage(named: self.iso2 + ".jpg")!
+    }
+
+    static let allCountries: [Country] = {
+        let countriesUrl = Bundle.main.url(forResource: "countries", withExtension: "json")!
+        let countriesData = try! Data(contentsOf: countriesUrl)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try! decoder.decode([Country].self, from: countriesData)
+    }()
 
 }
