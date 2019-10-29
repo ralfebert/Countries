3eab5caf5f7acfe365a6a5052026fcc331ef331d
Ralf Ebert <info@ralfebert.de>
2019-10-29 12:28:05 +0100

    Segue zu Country-Einzel-Ansicht vorbereitet

---
+++ Countries/Controller/Country+Maps.swift
@@ -0,0 +1,17 @@
+// © 2019 Ralf Ebert — iOS Example Project: Countries
+// License: https://opensource.org/licenses/MIT
+
+import Foundation
+import MapKit
+
+extension Country {
+
+    func openInMaps() {
+        let coordinates = CLLocationCoordinate2D(latitude: self.landmark.latitude, longitude: self.landmark.longitude)
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = self.landmark.name
+        mapItem.openInMaps()
+    }
+
+}
