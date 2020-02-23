334885d6cd370663b0383db996638f4dae086627
Ralf Ebert <info@ralfebert.de>
2020-02-23 13:31:50 +0100

    example with dynamic prototype table view cells

---
+++ Countries/Controller/CountriesTableViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class CountriesTableViewController: UITableViewController {
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
+
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+    
+}
