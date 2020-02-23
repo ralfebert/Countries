53c5ff5565084805e6cb68dc531851cc38fccef1
Ralf Ebert <info@ralfebert.de>
2020-02-23 13:47:08 +0100

    tvc showing countries

--- Countries/Controller/CountriesTableViewController.swift
+++ Countries/Controller/CountriesTableViewController.swift
@@ -1,27 +1,37 @@
 import UIKit
 
+struct Country {
+    var isoCode: String
+    var name: String
+}
+
 class CountriesTableViewController: UITableViewController {
+
+        let countries = [
+        Country(isoCode: "at", name: "Austria"),
+        Country(isoCode: "be", name: "Belgium"),
+        Country(isoCode: "de", name: "Germany"),
+        Country(isoCode: "el", name: "Greece"),
+        Country(isoCode: "fr", name: "France"),
+    ]
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return countries.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        let country = countries[indexPath.row]
+        cell.textLabel?.text = country.name
 
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
-    }
-    
 }
