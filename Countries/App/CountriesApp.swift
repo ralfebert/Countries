e9a226a68c3e07f0a103f485955628ac9c3a596c
Ralf Ebert <info@ralfebert.de>
2021-07-27 16:14:07 +0200

    example adapted for imagedownload example

---
+++ Countries/App/CountriesApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import UIKit
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CountryListView()
+        }
+    }
+}
--- Countries/Views/CountryRowView.swift
+++ Countries/Views/CountryRowView.swift
@@ -1,16 +1,11 @@
-// © 2019 Ralf Ebert — iOS Example Project: Countries
-// License: https://opensource.org/licenses/MIT
-
 import SwiftUI
 
 struct CountryRowView: View {
-
     let country: Country
 
     var body: some View {
-
         HStack {
-            Image(uiImage: self.country.image)
+            Image("placeholder")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 50)
@@ -18,24 +13,17 @@ struct CountryRowView: View {
             Text(self.country.name)
             Spacer()
         }
-
     }
-
 }
 
 struct CountryRow_Previews: PreviewProvider {
     static var previews: some View {
-
         Group {
-
             CountryRowView(country: Country.allCountries[1])
                 .previewLayout(.fixed(width: 300, height: 44))
 
             CountryRowView(country: Country.allCountries[2])
                 .previewLayout(.fixed(width: 400, height: 80))
-
         }
-
     }
-
 }
