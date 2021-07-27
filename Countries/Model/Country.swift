9a684babefd75292b27ef211d726544dfc40feb9
Ralf Ebert <info@ralfebert.de>
2021-07-27 16:14:07 +0200

    example adapted for imagedownload example

--- Countries/Model/Country.swift
+++ Countries/Model/Country.swift
@@ -1,9 +1,5 @@
-// © 2019 Ralf Ebert — iOS Example Project: Countries
-// License: https://opensource.org/licenses/MIT
-
 import CoreLocation
 import Foundation
-import UIKit
 
 struct Landmark: Codable {
     var name: String
@@ -14,21 +10,16 @@ struct Landmark: Codable {
     var copyright: String
 
     var coordinate: CLLocationCoordinate2D {
-        return CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
 
 struct Country: Codable, Identifiable {
+    var id: String { iso2 }
 
-    var id: String { self.iso2 }
-    
     var iso2: String
     var name: String
     var landmark: Landmark
-
-    var image: UIImage {
-        UIImage(named: self.iso2)!
-    }
 
     static let allCountries: [Country] = {
         let countriesUrl = Bundle.main.url(forResource: "countries", withExtension: "json")!
@@ -37,5 +28,4 @@ struct Country: Codable, Identifiable {
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         return try! decoder.decode([Country].self, from: countriesData)
     }()
-
 }
--- Countries/Views/CountryListView.swift
+++ Countries/Views/CountryListView.swift
@@ -1,24 +1,15 @@
-// © 2019 Ralf Ebert — iOS Example Project: Countries
-// License: https://opensource.org/licenses/MIT
-
 import SwiftUI
 
 struct CountryListView: View {
-
     let countries = Country.allCountries
 
     var body: some View {
-
         NavigationView {
             List(countries) { country in
-                NavigationLink(destination: CountryView(country: country)) {
-                    CountryRowView(country: country)
-                }
+                CountryRowView(country: country)
             }
-            .navigationBarTitle("Länder")
+            .navigationBarTitle("Countries")
         }
-        .navigationViewStyle(StackNavigationViewStyle())
-
     }
 }
 
